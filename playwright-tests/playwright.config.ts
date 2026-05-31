64d2ce6ed95d14193e4252b559d3394055c11045
Nataliia_Lushnytska <natalochka2509@gmail.com>
2026-05-31 23:56:14 +0300

    Add files via upload

---
+++ playwright-tests/playwright.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './src/tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: [
+    ['html'],
+    ['json', { outputFile: 'test-results/results.json' }],
+    ['allure-playwright'],
+  ],
+  use: {
+    baseURL: 'https://www.greencity.cx.ua/#/greenCity',
+    trace: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+  webServer: undefined,
+});
